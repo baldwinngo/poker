79ac2efed604cebd332c7083be99896c786af343
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-11-08 21:32:35 -0800

    simple player functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [stack, setStack] = useState(100)
+  const [betSize, setBetSize] = useState(0)
+  const [pot, setPot] = useState(0)
+
+
+  const handleBetSize = (e: any) => {
+    e.preventDefault()
+    setBetSize(e.target.value)
+  }
+  
+  const handleBet = (e: any) => {
+    e.preventDefault()
+    console.log(typeof pot)
+    let updatePot = pot + betSize
+    setPot(updatePot)
+    setStack(stack - betSize)
+  }
+
   return (
     <div className="App">
-      hello
+      <div>
+        pot { pot }
+      </div>
+      <div>
+        player { stack }
+      </div>
+      <div>
+        bet size: <input value={ betSize } onChange={ handleBetSize }/>
+        <button type="submit" onClick={ handleBet }>bet</button>
+      </div>
     </div>
   );
 }
